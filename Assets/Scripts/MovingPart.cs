57c5961a0591e733a3c3fbd45c59e2b6839f48df
Bogdan Tănăsoiu <bogdan.tana@gmail.com>
2018-01-26 23:52:46 +0200

    choo choo

---
+++ Assets/Scripts/MovingPart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPart : MonoBehaviour
+{
+	private void Update()
+	{
+		transform.Translate(Vector3.forward * Time.deltaTime, Space.Self);
+	}
+}
---
+++ Assets/Scripts/Tunnel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tunnel : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject locomotive;
+
+	[SerializeField]
+	private GameObject wagon;
+
+	[SerializeField]
+	private float spawnTime;
+
+	private void Awake()
+	{
+		SpawnTrain(4);
+	}
+
+	public void SpawnTrain(int wagonCount)
+	{
+		StartCoroutine(SpawnTrainCoroutine(wagonCount));
+	}
+
+	public void SetColor(Color color)
+	{
+
+	}
+
+	private IEnumerator SpawnTrainCoroutine(int wagonCount)
+	{
+		var newLocomotive = Instantiate(locomotive, transform.position, transform.rotation);
+
+		for (int i = 0; i < wagonCount; i++)
+		{
+			yield return new WaitForSeconds(spawnTime);
+			Instantiate(wagon, transform.position, transform.rotation);
+		}
+	}
+}
