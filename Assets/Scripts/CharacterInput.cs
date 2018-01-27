c7721e0500fb3a0c5dbd3e952cd445e24073048a
Bogdan Tănăsoiu <bogdan.tana@gmail.com>
2018-01-27 23:19:24 +0200

    x360 control, characterinput

---
+++ Assets/Scripts/CharacterInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class CharacterInput : MonoBehaviour
+{
+	[SerializeField]
+	XboxController controller;
+
+	float xAxis, yAxis;
+
+	float speed = 5f;
+
+	public void Initialize(XboxController c)
+	{
+		controller = c;
+	}
+
+	void Update()
+	{
+		xAxis = XCI.GetAxis(XboxAxis.LeftStickX, controller);
+		yAxis = XCI.GetAxis(XboxAxis.LeftStickY, controller);
+
+		Vector3 dir = new Vector3(xAxis, 0f, yAxis).normalized;
+		if (dir.magnitude == 1f)
+		{
+			transform.forward = dir;
+		}
+
+		transform.Translate(Vector3.forward * dir.magnitude * Time.deltaTime * speed, Space.Self);
+		//transform.Translate(dir * Time.deltaTime * speed);
+	}
+}
