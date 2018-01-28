3a456255c10e51dc4e215f0e0b493bef98e3c9dc
Bogdan Tănăsoiu <bogdan.tana@gmail.com>
2018-01-28 05:39:12 +0200

    main menu

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class Main : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject charPrefab;
+
+	[SerializeField]
+	private Transform[] spawnPoints;
+
+	void Awake()
+	{
+			if (PlayerPrefs.GetInt("player1") > 0)
+			{
+				var newChar = Instantiate(
+					charPrefab,
+					spawnPoints[0].position,
+					Quaternion.identity);
+
+				newChar.GetComponent<CharacterInput>().Initialize(XboxController.First);
+			}
+
+			if (PlayerPrefs.GetInt("player2") > 0)
+			{
+				var newChar = Instantiate(
+					charPrefab,
+					spawnPoints[1].position,
+					Quaternion.identity);
+
+					
+				newChar.GetComponent<CharacterInput>().Initialize(XboxController.Second);
+			}
+
+			if (PlayerPrefs.GetInt("player3") > 0)
+			{
+				var newChar = Instantiate(
+					charPrefab,
+					spawnPoints[2].position,
+					Quaternion.identity);
+
+				newChar.GetComponent<CharacterInput>().Initialize(XboxController.Third);
+			}
+
+			if (PlayerPrefs.GetInt("player4") > 0)
+			{
+				var newChar = Instantiate(
+					charPrefab,
+					spawnPoints[3].position,
+					Quaternion.identity);
+					
+				newChar.GetComponent<CharacterInput>().Initialize(XboxController.Fourth);
+			}
+	}
+}
---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using XboxCtrlrInput;
+
+public class MenuUI : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject[] players;
+
+	public void ActivatePlayer(int index)
+	{
+		players[index].SetActive(true);
+	}
+
+	void Awake()
+	{
+		PlayerPrefs.DeleteAll();
+	}
+
+	void Update()
+	{
+		if (XCI.GetButtonDown(XboxButton.Start, XboxController.Any))
+		{
+			float playerCount = 0f;
+			if (PlayerPrefs.GetInt("player1") > 0) playerCount++;
+			if (PlayerPrefs.GetInt("player2") > 0) playerCount++;
+			if (PlayerPrefs.GetInt("player3") > 0) playerCount++;
+			if (PlayerPrefs.GetInt("player4") > 0) playerCount++;
+
+			if (playerCount > 1)
+			{
+				SceneManager.LoadScene("Environment");
+			}
+		}
+
+		if (XCI.GetButtonDown(XboxButton.A, XboxController.First))
+		{
+			PlayerPrefs.SetInt("player1", 1);
+			ActivatePlayer(0);
+		}
+
+		if (XCI.GetButtonDown(XboxButton.A, XboxController.Second))
+		{
+			PlayerPrefs.SetInt("player2", 1);
+			ActivatePlayer(1);
+		}
+
+		if (XCI.GetButtonDown(XboxButton.A, XboxController.Third))
+		{
+			PlayerPrefs.SetInt("player3", 1);
+			ActivatePlayer(2);
+		}
+
+		if (XCI.GetButtonDown(XboxButton.A, XboxController.Fourth))
+		{
+			PlayerPrefs.SetInt("player4", 1);
+			ActivatePlayer(3);
+		}
+	}
+}
